ae87dce49ef71bbaf3aecf0877cb2070cc4fc99b
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2024-01-04 22:46:59 -0300

    Sign in interface and dashboard layout

---
+++ src/components/account-menu.tsx
@@ -0,0 +1,44 @@
+import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+import { Building, ChevronDown, LogOut } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          Pizza Shop
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Rodrigo Teixeira</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            rodrigoteix1998@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer">
+          <Building className="mr-2 h-4 w-4" />
+          <span>Perfil da loja</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="dark: cursor-pointer text-rose-400 text-rose-500 ">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
